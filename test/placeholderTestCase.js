d96066f56627abdb8b08e1ef42ccd7b44e7db5d4
Gabriel Correia <gmfc@users.noreply.github.com>
2016-04-20 11:59:22 -0300

    Create placeholderTestCase.js

---
+++ test/placeholderTestCase.js
@@ -0,0 +1,17 @@
+var assert = require("assert");
+
+describe('Deve dar certo', function() {
+    
+    var resultado;
+    beforeEach(function() {
+        var resultado = true;
+        console.log("Antes");
+    });
+    
+    
+    it('Da cerrto por padrao', function() {
+        assert.equal(resultado, true);
+    });
+    
+    
+});
