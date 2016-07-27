2adbcbfc8c732414d6b1fdb75aff551ccd5a83e7
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2016-07-27 15:34:44 -0300

    Novos testes para classe Raw

--- test/rawDataTestCase.js
+++ test/rawDataTestCase.js
@@ -1,25 +1,53 @@
-var assert = require("assert");
-
+var should = require('should');
 var Raw = require('../js/rawData.js');
 
-describe('Teste da classe Raw', function() {
-    
-    var strings;
-    before('populando carga',function() {
-        
-        strings = "123,0.1,1,2.2";
+
+describe('Testes da classe Raw', function() {
+    var strings = [];
+    var numbers = [];
+    var nDeLinhas = 20000;
+    this.timeout(20000);
+    before('populando linhas...',function() {
+        for(var i=0;i<nDeLinhas;i++){
+            var TR = Math.random(),
+                TL = Math.random(),
+                BR = Math.random(),
+                BL = Math.random();
+            numbers[i] = [TR,TL,BR,BL];
+            strings[i] = TR + "," + TL + "," + BR + "," + BL;
+            //console.log(numbers[i][0] + " - " + TR);
+        }
     });
     
     
-    it('Popula dados e testa se esta correto', function() {
+    it('Phrase test ' + nDeLinhas + ' linhas', function() {
         var rawTest = new Raw();
-        rawTest.pushData(strings);
-        assert.strictEqual(rawTest.TR[0], 123);
-        assert.strictEqual(rawTest.TL[0], 0.1);
-        assert.strictEqual(rawTest.BR[0], 1);
-        assert.strictEqual(rawTest.BL[0], 2.2);
-        //assert.
+        for(var i=0;i<nDeLinhas;i++){
+            rawTest.pushData(strings[i]);
+        }
+        
+        for(var i=0;i<nDeLinhas;i++){
+            rawTest.TR[i].should.be.exactly(numbers[i][0]);
+            rawTest.TL[i].should.be.exactly(numbers[i][1]);
+            rawTest.BR[i].should.be.exactly(numbers[i][2]);
+            rawTest.BL[i].should.be.exactly(numbers[i][3]);
+            
+        }
+        
     });
+    
+    it('Calcula Fx12,Fx34,Fx14,Fx23');
+    it('Calcula CPx e CPy');
+    it('Calcula Deslocamento da oscilação total, DOT');
+    it('Calcula Desvio padrão (AP e ML)'); 
+    it('Calcula RMS (Root Mean Square) AP e ML');
+    it('Calcula Velocidade média (VM)');
+    it('Calcula Velocidade média (VM)');
+    it('Calcula Amplitude de deslocamento do CP');
+    it('Calcula Velocidade média total (VMT)');
+    it('Calcula Área');
+    
+    
     
     
 });
