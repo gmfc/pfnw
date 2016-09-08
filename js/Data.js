df07e43220b993772fed0bb789ef81c5d29a9380
gabriel.mfcorreia <gabriel.mfcorreia@gmail.com>
2016-09-08 10:55:46 -0400

    save incompleto

--- js/Data.js
+++ js/Data.js
@@ -1,65 +1,68 @@
+'use strict'
 /*
  *  Classe responsavel por administrar e receber os dados da plataforma
  *
  */
 function PlatData() {
-	
+
 	this.az0 = 0;
 	this.a = 24.76; //Medida centro -> lteral
 	this.b = 15.24; //Medida centro -> topo
-	
-    // arrays do cada sensor
-    this.TR = []; // top right
-    this.TL = []; // top left
-    this.BR = []; // bottom right
-    this.BL = []; // bottom left
-	
+
+	// arrays de cada sensor
+	this.TR = []; // top right
+	this.TL = []; // top left
+	this.BR = []; // bottom right
+	this.BL = []; // bottom left
+
 	this.FxTRL = [];
 	this.FxBLR = [];
-	
+
 	this.FxTBR = [];
 	this.FxTBL = [];
-	
+
 	this.CPx = [];
 	this.CPy = [];
+
+	//this.DOT = 0;
 
 }
 
 
 // push leitura. Recebe String com leitura dos dados no ciclo
 // e adiciona no final de cada array;
-PlatData.prototype.pushData = function (data) {
+PlatData.prototype.pushData = function(data) {
 
-    var arr = data.split(",").map(function (val) {
-        return Number(val);
-    });
-    this.TR.push(arr[0]);
-    this.TL.push(arr[1]);
-    this.BR.push(arr[2]);
-    this.BL.push(arr[3]);
+	var arr = data.split(";").map(function(val) {
+		return Number(val);
+	});
+	this.TR.push(arr[0]);
+	this.TL.push(arr[1]);
+	this.BR.push(arr[2]);
+	this.BL.push(arr[3]);
 };
 
-PlatData.prototype.getTR = function () {
-    return this.TR;
-};
-
-
-PlatData.prototype.getTL = function () {
-    return this.TL;
+PlatData.prototype.getTR = function() {
+	return this.TR;
 };
 
 
-PlatData.prototype.getBR = function () {
-    return this.BR;
+PlatData.prototype.getTL = function() {
+	return this.TL;
 };
 
 
-PlatData.prototype.getBL = function () {
-    return this.BL;
+PlatData.prototype.getBR = function() {
+	return this.BR;
 };
 
-PlatData.prototype.calcFx = function () {
-	for(var i=0;i<this.BR.length;i++){
+
+PlatData.prototype.getBL = function() {
+	return this.BL;
+};
+
+PlatData.prototype.calcFx = function() {
+	for (var i = 0; i < this.BR.length; i++) {
 		this.FxTRL[i] = this.TR[i] + this.TL[i];
 		this.FxBLR[i] = this.BL[i] + this.BR[i];
 		this.FxTBR[i] = this.TR[i] + this.BR[i];
@@ -67,26 +70,85 @@ PlatData.prototype.calcFx = function () {
 	}
 };
 
-PlatData.prototype.calcCOP = function () {
-	for(var i=0;i<this.BR.length;i++){
-		this.CPx[i] = this.fax(this.TR[i],this.TL[i],this.BL[i],this.BR[i],this.az0,this.FxTRL[i],this.FxBLR[i]);
-		this.CPy[i] = this.fay(this.TR[i],this.TL[i],this.BL[i],this.BR[i],this.az0,this.FxTBR[i],this.FxTBL[i]);
+PlatData.prototype.calcCOP = function() {
+	for (var i = 0; i < this.BR.length; i++) {
+		this.CPx[i] = fax(this.a, this.TR[i], this.TL[i], this.BL[i], this.BR[i], this.az0, this.FxTRL[i], this.FxBLR[i]);
+		this.CPy[i] = fay(this.b, this.TR[i], this.TL[i], this.BL[i], this.BR[i], this.az0, this.FxTBR[i], this.FxTBL[i]);
 	}
 };
 
-PlatData.prototype.fax = function (fz1,fz2,fz3,fz4,az0,fx12,fx34) {
-	var t1 = this.a*(-fz1+fz2+fz3-fz4);
-	var t3 = fz1+fz2+fz3+fz4;
-	var t2 = az0*(fx12+fx34);
-	return (-t1-t2)/t3;
-};
+PlatData.prototype.calcDOT = function() {
+	this.DOT = 0;
+	for (let i = 0; i < this.CPx.length; i++) {
+		this.DOT += Math.sqrt(Math.pow(this.CPx[i], 2) + Math.pow(this.CPy[i], 2));
+	}
+}
+// AP = y ML = x
+PlatData.prototype.calcDEV = function() {
+	this.DevAP = standardDeviation(this.CPy);
+	this.DevML = standardDeviation(this.CPx);
+}
 
-PlatData.prototype.fay = function (fz1,fz2,fz3,fz4,az0,fy14,fy23) {
-	var t1 = this.b*(fz1+fz2-fz3-fz4);
-	var t3 = fz1+fz2+fz3+fz4;
-	var t2 = az0*(fy14+fy23);
-	return (t1+t2)/t3;
-};
+PlatData.prototype.calcRMS = function() {
+	
+	// calc AP(y)
+	let sumAP = 0;
+	for(let i=0;i<this.CPy.length;i++){
+		sumAP = this.CPy[i]*this.CPy[i];
+	}
+	
+	// calc ML(x)
+	let sumML = 0;
+	for(let i=0;i<this.CPx.length;i++){
+		sumML = this.CPx[i]*this.CPx[i];
+	}
+
+	this.rmsAP = Math.sqrt(sumAP/this.CPy.length);
+	this.rmsML = Math.sqrt(sumML/this.CPx.length);
+}
+
+//////
+// HELPERS
+/////
+
+function fax(a, fz1, fz2, fz3, fz4, az0, fx12, fx34) {
+	var t1 = a * (-fz1 + fz2 + fz3 - fz4);
+	var t3 = fz1 + fz2 + fz3 + fz4;
+	var t2 = az0 * (fx12 + fx34);
+	return (-t1 - t2) / t3;
+}
+
+function fay(b, fz1, fz2, fz3, fz4, az0, fy14, fy23) {
+	var t1 = b * (fz1 + fz2 - fz3 - fz4);
+	var t3 = fz1 + fz2 + fz3 + fz4;
+	var t2 = az0 * (fy14 + fy23);
+	return (t1 + t2) / t3;
+}
+
+
+function standardDeviation(values) {
+	var avg = average(values);
+
+	var squareDiffs = values.map(function(value) {
+		var diff = value - avg;
+		var sqrDiff = diff * diff;
+		return sqrDiff;
+	});
+
+	var avgSquareDiff = average(squareDiffs);
+
+	var stdDev = Math.sqrt(avgSquareDiff);
+	return stdDev;
+}
+
+function average(data) {
+	var sum = data.reduce(function(sum, value) {
+		return sum + value;
+	}, 0);
+
+	var avg = sum / data.length;
+	return avg;
+}
 
 
 
---
+++ test/DataRandomTestCase.js
@@ -0,0 +1,89 @@
+'use strict'
+var should = require('should');
+var PlatData = require('../js/Data.js');
+
+
+describe('Testes da classe Data com dados aleatorios', function() {
+	var strings = [];
+	var numbers = [];
+	var nDeLinhas = 20000;
+	this.timeout(20000);
+	var rawTest = new PlatData();
+	
+	before('populando linhas...', function() {
+		for (var i = 0; i < nDeLinhas; i++) {
+			var TI =  Math.floor(Math.random() * 8) + 5,
+				TR = Math.random()*100,
+				TL = Math.random()*100,
+				BR = Math.random()*100,
+				BL = Math.random()*100;
+			numbers[i] = [TR, TL, BR, BL];
+			strings[i] = TR + ";" + TL + ";" + BR + ";" + BL;
+		}
+	});
+
+
+	it('Phrase test ' + nDeLinhas + ' linhas', function() {
+
+		for (let i = 0; i < nDeLinhas; i++) {
+			rawTest.pushData(strings[i]);
+		}
+
+		for (let i = 0; i < nDeLinhas; i++) {
+			rawTest.TI[i].should.be.exactly(numbers[i][0]);
+			rawTest.TR[i].should.be.exactly(numbers[i][1]);
+			rawTest.TL[i].should.be.exactly(numbers[i][2]);
+			rawTest.BR[i].should.be.exactly(numbers[i][3]);
+			rawTest.BL[i].should.be.exactly(numbers[i][4]);
+
+		}
+
+	});
+
+	it('Calcula Fx12,Fx34,Fx14,Fx23', function() {
+		rawTest.calcFx();
+		for (var i = 0; i < rawTest.TR.length; i++) {
+			rawTest.FxTRL[i].should.be.Number();
+			rawTest.FxBLR[i].should.be.Number();
+			rawTest.FxTBR[i].should.be.Number();
+			rawTest.FxTBL[i].should.be.Number();
+		}
+	});
+
+	it('Calcula CPx e CPy', function() {
+		rawTest.calcCOP();
+		for (var i = 0; i < rawTest.TR.length; i++) {
+			rawTest.CPx[i].should.be.Number();
+			rawTest.CPy[i].should.be.Number();
+		}
+	});
+
+
+	it('Calcula Deslocamento da oscilação total, DOT', function() {
+		rawTest.calcDOT();
+		rawTest.DOT.should.be.Number();
+	});
+
+	it('Calcula Desvio padrão (AP e ML)', function() {
+		rawTest.calcDEV();
+		rawTest.DevAP.should.be.Number();
+		rawTest.DevML.should.be.Number();
+	});
+	
+	it('Calcula RMS (Root Mean Square) AP e ML', function() {
+		rawTest.calcRMS();
+		rawTest.rmsAP.should.be.Number();
+		rawTest.rmsML.should.be.Number();
+	});
+	
+	it('Calcula Frequencia');
+	
+	it('Calcula Velocidade média (VM)');
+	it('Calcula Amplitude de deslocamento do CP');
+	it('Calcula Velocidade média total (VMT)');
+	it('Calcula Área');
+
+
+
+
+});
