fad66010d791f0783138857e6a2b63e1d5d2284d
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2016-09-21 12:58:56 -0300

    placeholderTestCase fix

--- test/placeholderTestCase.js
+++ test/placeholderTestCase.js
@@ -1,8 +1,8 @@
-var assert = require('assert');
-var should = require('should');
+var assert = require("assert");
+var should = require("should");
 
 
-describe('Teste do sistema de testes', function() {
+describe("Teste do sistema de testes", function() {
     
     var resultado;
     beforeEach(function() {
@@ -10,19 +10,19 @@ describe('Teste do sistema de testes', function() {
     });
     
     
-    it('Compara true com true... (ASSERT test)', function() {
+    it("Compara true com true... (ASSERT test)", function() {
         assert.equal(resultado, true);
     });
     
-    it('Verifica diferenca... (ASSERT test)', function() {
+    it("Verifica diferenca... (ASSERT test)", function() {
         assert.notEqual(resultado, false);
     });
     
-    it('Compara true com true... (SHOULD test)', function() {
+    it("Compara true com true... (SHOULD test)", function() {
         resultado.should.be.exactly(true);
     });
     
-    it('Verifica diferenca... (SHOULD test)', function() {
+    it("Verifica diferenca... (SHOULD test)", function() {
         resultado.should.not.be.exactly(false);
     });
     
