0505b61b3bf027c26112a4fbe23068ccbac955a0
Gabriel Correia <gabriel.mfcorreia@gmail.com>
2016-10-11 15:27:31 -0300

    Processo de deployment automatizado

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var buffer = require('vinyl-buffer');
 var rename = require('gulp-rename');
 var jsdoc = require('gulp-jsdoc3');
 var del = require('del');
+var NwBuilder = require('nw-builder');
+var pack = require('./package.json');
 
 gulp.task('rtcontrol', function() {
 	return browserify('./src/RealTimeController.js')
@@ -49,6 +51,24 @@ gulp.task('clean:js', function() {
 
 gulp.task('stream', function() {
 	gulp.watch('./src/**/*.js', ['build']);
+});
+
+gulp.task('dist:win', ['build'], function() {
+	var nw = new NwBuilder({
+		files: ['./ui/**/**'], // use the glob format
+		platforms: ['win32'],
+		appVersion: pack.version,
+		flavor: 'normal',
+		appName: 'Plataforma de Força',
+		buildType: 'versioned',
+		winIco: './assets/icon.ico'
+	});
+	nw.on('log', console.log);
+	nw.build().then(function() {
+		console.log('all done!');
+	}).catch(function(error) {
+		console.error(error);
+	});
 });
 
 gulp.task('build', ['clean:js', 'rtcontrol', 'controladorIndex']);
